6d91014b88c19602e60bead2abafb03eb1795ef5
asnganje <abd.nganje@gmail.com>
2023-03-06 20:44:04 +0300

    Added session controller logic

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,18 @@
 class Api::SessionsController < Devise::SessionsController
-  def new
-    user = User.find_by(username: params[:username])
 
-    if user && user.valid_password?(params[:password])
-      session[:user_id] = user.id
+  def new
+    user = User.find_by(username: sign_in_params[:username])
+    if user&.authenticate(sign_in_params[:password])
+      # sign in the user and redirect to the desired page
       render json: { message: "Logged in successfully!" }
     else
+      # handle sign-in failure
       render json: { error: "Wrong username or password" }, status: :unauthorized
     end
+  end
+
+  private
+  def sign_in_params
+    params.require(:user).permit(:username, :password)
   end
 end
