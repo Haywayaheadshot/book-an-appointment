8db09b811a8924e6ade91338a8498ae0e845ee1d
Kelvin-Ben <kevoben14@gmail.com>
2023-03-07 00:27:04 +0300

    Validate user password and fix linters

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,18 @@
 class Api::SessionsController < Devise::SessionsController
-
   def new
     user = User.find_by(username: sign_in_params[:username])
-    if user && user.valid_password?(sign_in_params[:password])
-    # if user&.authenticate(sign_in_params[:password])
+    if user&.valid_password?(sign_in_params[:password])
+      # if user&.authenticate(sign_in_params[:password])
       # sign in the user and redirect to the desired page
-      render json: { message: "Logged in successfully!" }
+      render json: { message: 'Logged in successfully!' }
     else
       # handle sign-in failure
-      render json: { error: "Wrong username or password" }, status: :unauthorized
+      render json: { error: 'Wrong username or password' }, status: :unauthorized
     end
   end
 
   private
+
   def sign_in_params
     params.require(:user).permit(:username, :password)
   end
