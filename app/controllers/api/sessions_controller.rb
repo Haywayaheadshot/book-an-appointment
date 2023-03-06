ee878b301680a630f384e223da91dbf6f51652c3
asnganje <abd.nganje@gmail.com>
2023-03-06 20:08:07 +0300

    setup session controller for login

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,12 @@
-class Api::SessionsController < ApplicationController
+class Api::SessionsController < Devise::SessionsController
   def new
-    p 'API here'
+    user = User.find_by(username: params[:username])
+
+    if user && user.valid_password?(params[:password])
+      session[:user_id] = user.id
+      render json: { message: "Logged in successfully!" }
+    else
+      render json: { error: "Wrong username or password" }, status: :unauthorized
+    end
   end
 end
