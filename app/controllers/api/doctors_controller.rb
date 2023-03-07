8cf9ecb001cf6c5e349011c48a803876fa304847
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-07 20:05:10 +0000

    Configure doctors render to allowed desired parameters

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,6 +1,6 @@
 class Api::DoctorsController < ApplicationController
   def index
     @doctors = Doctor.all
-    render json: @doctors
+    render json: @doctors, only: [:name, :specialty, :years_of_experience, :bio, :age, :qualifications,:location_of_work, :photo ]
   end
 end
