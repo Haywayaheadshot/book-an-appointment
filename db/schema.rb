f833559929b29467b1b065613a4eb10d075119ce
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-03-07 00:35:29 +0000

    updated schema with doctors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_124914) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_003039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_124914) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.string "years_of_experience"
+    t.text "bio"
+    t.integer "age"
+    t.text "achievements"
+    t.string "location_of_work"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
